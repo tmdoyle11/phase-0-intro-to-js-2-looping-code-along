b1dd46ef805b0b0906574b8dec34a577b967eb9e
Thomas Doyle <tmdoyle11@gmail.com>
2022-03-02 12:59:02 -0600

    commit tree 2e9fa63fbb94bab2eeb2ee8a2a0dc2940996c21a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const arr1 = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        arr1.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        debugger;}
+        return arr1;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--
+    }
+}
